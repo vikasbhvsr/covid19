503e943b2d351da54a1d8dd3a6268064e2b95b05
Vikas Bhavsar <vikasbhvsr@gmail.com>
2020-03-16 21:06:09 -0700

    illinois added

--- public/js/index.js
+++ public/js/index.js
@@ -61,3 +61,35 @@ fetch('https://covid19.mathdro.id/api/confirmed')
 		`;
     california.insertAdjacentHTML('beforeend', html);
   });
+
+fetch('https://covid19.mathdro.id/api/confirmed')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    const { confirmed, recovered, deaths } = data[31];
+    const illinois = document.querySelector('.illinois .data');
+    const html = `
+		<div class="grid gap-6 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 grid-cols-1 justify-center">
+			<div class="shadow-lg rounded-lg p-5 hover:shadow-xl bg-yellow-300 text-center">
+				<h2 class="uppercase text-lg font-bold text-xl tracking-normal pb-3">Confirmed</h2>
+				<p class="text-5xl">🤢</p>
+				<p class="text-5xl">${data[81].confirmed.toLocaleString()}</p>
+			</div>
+			<div class="shadow-lg rounded-lg p-5 hover:shadow-xl bg-green-300 text-center">
+				<h2 class="uppercase text-lg font-bold text-xl tracking-normal pb-3">Recovered</h2>
+				<p class="text-5xl">👍🏾</p>
+				<p class="text-5xl">${data[81].recovered.toLocaleString()}</p>
+			</div>
+			<div class="shadow-lg rounded-lg p-5 hover:shadow-xl bg-red-300 text-center">
+				<h2 class="uppercase text-lg font-bold text-xl tracking-normal pb-3">Deaths</h2>
+				<p class="text-5xl">👼</p>
+				<p class="text-5xl">${data[81].deaths.toLocaleString()}</p>
+			</div>
+		</div>
+		<div class="text-left my-4">
+			<small>Last Update: ${data[81].lastUpdate}</small>
+		</div>
+		`;
+    illinois.insertAdjacentHTML('beforeend', html);
+  });
